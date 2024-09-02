a41d46050f4229f3a0f1df92d5b560214af67408
mattalajas <mattalajas@gmail.com>
2024-09-02 18:53:29 +1200

    models

---
+++ download.sh
@@ -0,0 +1,9 @@
+conda install pytorch-sparse -c pyg -y
+conda install pytorch-scatter -c pyg -y
+conda install pytorch-cluster -c pyg -y
+conda install pytorch-spline-conv -c pyg -y
+conda install pyg -c pyg -y
+conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
+conda install matplotlib numpy networkx pandas scikit-learn scipy seaborn tensorboard -y
+conda install -c conda-forge pyarrow -y
+pip install jupyterlab
