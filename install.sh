613a590b7670faa377610503db7417ccd999d999
mattalajas <mattalajas@gmail.com>
2025-07-29 16:56:43 +1200

    changes

--- install.sh
+++
@@ -1,6 +0,0 @@
-pip install torch==2.1.0 --index-url https://download.pytorch.org/whl/cu121
-pip install pandas==1.3.5
-pip install pyg_lib==0.3.1 torch_scatter==2.1.2 torch_sparse==0.6.18 torch_cluster==1.6.3 torch_spline_conv==1.2.2 -f https://data.pyg.org/whl/torch-2.1.0+cu121.html
-pip install matplotlib scikit-learn ipykernel tensorboard omegaconf tables seaborn torch-geometric-temporal networkx
-pip install git+https://github.com/TorchSpatiotemporal/tsl.git 
-pip install hydra-core POT numba
