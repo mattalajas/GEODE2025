a34abeb0adaf793c716bbedb1bd52e77c4ff1094
mattalajas <mattalajas@gmail.com>
2025-07-29 16:45:15 +1200

    changes

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-pip install torch==2.5.1 --index-url https://download.pytorch.org/whl/cu124
+pip install torch==2.1.0 --index-url https://download.pytorch.org/whl/cu121
 pip install pandas==1.3.5
-pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.5.1+cu124.html
-pip install matplotlib scikit-learn ipykernel tensorboard haversine omegaconf tables seaborn torch-geometric-temporal networkx
+pip install pyg_lib==0.3.1 torch_scatter==2.1.2 torch_sparse==0.6.18 torch_cluster==1.6.3 torch_spline_conv==1.2.2 -f https://data.pyg.org/whl/torch-2.1.0+cu121.html
+pip install matplotlib scikit-learn ipykernel tensorboard omegaconf tables seaborn torch-geometric-temporal networkx
 pip install git+https://github.com/TorchSpatiotemporal/tsl.git 
 pip install hydra-core POT numba
